6d1b108626a499c1d850a9e09cb8aafc10953813
Iliyan-Y <maximizebg@gmail.com>
2023-02-11 09:00:21 +0000

    server splited to mudles

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 
 struct AppState {
   app_name: String,
+  deployment_env: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,19 +28,28 @@ async fn manual_hello(data: web::Data<AppState>) -> impl Responder {
   HttpResponse::Ok().body(res)
 }
 
+fn state_config(cfg: &mut web::ServiceConfig) {
+  cfg.app_data(web::Data::new(AppState {
+    app_name: "Actix Test Api".to_string(),
+    deployment_env: "dev".to_string(),
+  }));
+}
+
+fn route_config(cfg: &mut web::ServiceConfig) {
+  cfg
+    .route("/hi", web::get().to(manual_hello))
+    .service(hello)
+    .service(echo);
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
-    App::new()
-      .service(
-        web::scope("/api/v1")
-          .app_data(web::Data::new(AppState {
-            app_name: "Actix Test Api".to_string(),
-          }))
-          .service(echo)
-          .route("/hey", web::get().to(manual_hello)),
-      )
-      .service(hello)
+    App::new().service(
+      web::scope("/api/v1")
+        .configure(state_config)
+        .configure(route_config),
+    )
   })
   .bind(("127.0.0.1", 3000))?
   .run()
