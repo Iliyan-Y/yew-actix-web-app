2c64a3798de7332e3a19b52dace1700c18afbb92
Iliyan-Y <maximizebg@gmail.com>
2023-02-11 17:16:04 +0000

    readme updated

--- backend/src/controllers/index_controller.rs
+++ backend/src/controllers/index_controller.rs
@@ -2,6 +2,6 @@ use actix_web::web::Query;
 
 use crate::routes::index::GetIndexParams;
 
-pub fn get_index(params: Query<GetIndexParams>) {
-  format!("HELOO BACKEND {:?}", params.test);
+pub fn get_index(params: Query<GetIndexParams>) -> String {
+  format!("HELOO BACKEND {:?}", params.test)
 }
