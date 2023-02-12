8f8f3caceb6b7a594af2686226ad655c2dfeddb9
Iliyan-Y <maximizebg@gmail.com>
2023-02-12 20:06:00 +0000

    sea-orm configured

--- backend/src/controllers/index_controller.rs
+++ backend/src/controllers/index_controller.rs
@@ -3,5 +3,5 @@ use actix_web::web::{Data, Query};
 use crate::{routes::index::IndexGetParams, AppState};
 
 pub fn get_index(params: Query<IndexGetParams>, data: Data<AppState>) -> String {
-  format!("HELOO BACKEND {:?}, {}", params.test, data.app_name)
+  format!("HELlO BACKEND {:?}, {}", params.test, data.app_name)
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,31 +1,51 @@
 use actix_web::{web, App, HttpServer};
 use routes::index::index_route_config;
+use sea_orm::{Database, DatabaseConnection};
 
 mod controllers;
 mod routes;
 
+#[derive(Debug, Clone)]
 pub struct AppState {
   app_name: String,
-  deployment_env: String,
+  db: DatabaseConnection,
 }
 
-fn state_config(cfg: &mut web::ServiceConfig) {
-  cfg.app_data(web::Data::new(AppState {
-    app_name: "Actix Test Api".to_string(),
-    deployment_env: "dev".to_string(),
-  }));
+async fn make_db_pool() -> DatabaseConnection {
+  let db_url = std::env::var("DATABASE_URL").unwrap();
+  let db = Database::connect(db_url).await.unwrap();
+  // let db_name = std::env::var("DB_NAME").unwrap();
+
+  // db.execute(Statement::from_string(
+  //   db.get_database_backend(),
+  //   format!("DROP DATABASE IF EXISTS \"{}\";", db_name),
+  // ))
+  // .await
+  // .unwrap();
+
+  return db;
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| {
+  pretty_env_logger::init();
+  dotenv::dotenv().ok();
+
+  let state = AppState {
+    app_name: "Actix Test Api".to_string(),
+    db: make_db_pool().await,
+  };
+
+  HttpServer::new(move || {
     App::new().service(
       web::scope("/api/v1")
-        .configure(state_config)
-        .configure(index_route_config),
+        .app_data(web::Data::new(state.clone()))
+        .configure(index_route_config), //  .wrap(middleware::Logger::default()),
     )
   })
   .bind(("127.0.0.1", 3000))?
   .run()
-  .await
+  .await?;
+
+  Ok(())
 }
